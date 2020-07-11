b9faf12fd2170aa8a3466fae2b8d7ca80dacc54c
manuelpcx <jesus.medina05@gmail.com>
2020-07-11 01:58:52 -0400

    problemas con los fontawesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,11 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'stylesheets')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'scss')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'fontawesome-free')
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
